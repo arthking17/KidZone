83e7bc234015488200c6c9821a1f5d6e976de2bd
arthking17 <nguesseuarthur17@gmail.com>
2021-09-01 12:58:33 +0100

    first commit

---
+++ src/main/java/tn/kidzone/spring/KidzoneApplication.java
@@ -0,0 +1,13 @@
+package tn.kidzone.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidzoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidzoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/kidzone/spring/KidzoneApplicationTests.java
@@ -0,0 +1,12 @@
+package tn.kidzone.spring;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidzoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
